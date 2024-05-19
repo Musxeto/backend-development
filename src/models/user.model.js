8b11eb42e944412cc469dcd570f07cd4d501cfcf
Mus <138971833+Musxeto@users.noreply.github.com>
2024-05-19 12:32:43 +0500

    refresh token and access token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -26,11 +26,11 @@ const userSchema = new Schema(
       trim: true,
     },
     avatar: {
-      type: String, //cloud url
+      type: String, // cloud url
       required: true,
     },
     coverImage: {
-      type: String, //cloud url
+      type: String, // cloud url
     },
     watchHistory: [
       {
@@ -50,7 +50,7 @@ const userSchema = new Schema(
 );
 
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next;
+  if (!this.isModified("password")) return next();
 
   this.password = await bcrypt.hash(this.password, 10);
   next();
@@ -59,4 +59,33 @@ userSchema.pre("save", async function (next) {
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+      watchHistory: this.watchHistory,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
 export const User = mongoose.model("User", userSchema);
