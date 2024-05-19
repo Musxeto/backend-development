bfe2985d8ad108bbf026549721fc722c6e0b2dd0
Mus <138971833+Musxeto@users.noreply.github.com>
2024-05-19 09:02:27 +0500

    db connection approach 1

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import express from "express";
+
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Error: ", error);
+    throw error;
+  }
+})();
