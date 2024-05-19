8be93535ee2967789faaf4b23e24d60b19bf15ea
Mus <138971833+Musxeto@users.noreply.github.com>
2024-05-19 11:37:57 +0500

    user schema

---
+++ src/models/user.model.js
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      index: true,
+      trim: true,
+    },
+    avatar: {
+      type: String, //cloud url
+      required: true,
+    },
+    coverImage: {
+      type: String, //cloud url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is Required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
