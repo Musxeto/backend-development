1caa66f67ef091ae693f44755f4452ca5d539ef4
Mus <138971833+Musxeto@users.noreply.github.com>
2024-05-24 10:30:18 +0500

    learning controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,18 @@
 import asyncHandler from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok ",
-  });
+  //GET USER DETAILS FROM FRONT END
+  //VALIDATION - NOT EMPTY
+  //CHECK IF USER ALREADY EXISTS
+  // CHECK FOR IMAGES, AVATAR
+  //-> UPLOAD THEM TO CLOUDINARY AVATAR
+  //CREATE USER OBJECT - CREATE ENTRY IN DB
+  // REMOVE PASSWORD AND REFRESH TOKEN FIELD
+  // check for user creation
+  // return res
+
+  const { fullname, email, username, password } = req.body;
+  console.log("email:", email);
 });
 
 export { registerUser };
